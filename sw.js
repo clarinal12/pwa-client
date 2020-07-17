417485aebb627a808eaa28470ea310a7d409c693
Al Sagarino <sagarinoclarin@gmail.com>
2020-07-17 08:22:52 +0800

    updated sw

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
 /* eslint-disable no-restricted-globals */
 
 function handleSync(event) {
-  console.log('[Service Worker] Notification click Received.', event);
+  console.log('[Service Worker] Sync Received.', event);
+
+  const options = {
+    data:
+      'https://developers.google.com/web/fundamentals/push-notifications/display-a-notification#icon',
+    body: 'Nice Body',
+    icon: 'https://via.placeholder.com/128/ff0000',
+    vibrate: [200, 100, 200],
+    tag: event.tag,
+    badge: 'https://via.placeholder.com/128/ff0000',
+    actions: [
+      {
+        action: 'Detail',
+        title: 'View',
+      },
+    ],
+  };
+
+  event.waitUntil(self.registration.showNotification('Nice Title', options));
 
   // event.notification.close();
   // event.waitUntil(clients.openWindow(event.notification.data));
